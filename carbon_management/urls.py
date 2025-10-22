8c516590ca55979b7622bfedf7f949cacca264c2
chenk7 <chenk7@knownsec.com>
2025-10-22 10:17:24 +0800

    Initial commit

--- carbon_management/urls.py
+++ carbon_management/urls.py
@@ -31,4 +31,4 @@ urlpatterns += i18n_patterns(
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
